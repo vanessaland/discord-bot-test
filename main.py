e9e1f458310e8370f5b49230f3838772964641d3
vanessaland <>
2021-05-01 20:45:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import os
+import discord
+from stay_awake import stay_awake
+
+stay_awake()
+
+bot = discord.Client()
+
+@bot.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(bot))
+
+@bot.event
+async def on_message(message):
+  if message.author == bot:
+    return
+
+  if message.content.startswith('$bussin'):
+    await message.channel.send('sheeeeeeesh')
+
+  if message.content.startswith('$echo'):
+    await message.channel.send(message.content[5:].strip())
+
+  if message.content.startswith('$hbd'):
+    if len(message.mentions) == 0:
+      await message.channel.send('Happy birthday, <@' + str(message.author.id) + '>!')
+    else:
+      for mention in message.mentions:
+        await message.channel.send('Happy birthday, <@' + str(mention.id)+'>!')
+
+  if message.content.startswith('$channel'):
+    channel_name = message.content[8:].strip()
+    await message.channel.category.create_text_channel(channel_name)
+
+  
+
+bot.run(os.environ['TOKEN'])
---
+++ stay_awake.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+
+def home():
+  return "Hey, I'm awake!"
+
+def run():
+  app.run(host='0.0.0.0', port=800)
+
+def stay_awake():
+  t = Thread(target=run)
+  t.start()
+
